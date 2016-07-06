d07877b297b9dc89fdae88371d1adbeb07b2730f
bgrounds <bgrounds@ea.com>
2016-07-06 15:06:43 -0700

    various changes

--- storage.js
+++ storage.js
@@ -1,33 +1,42 @@
 /**
- * Template for a function module that can be served by serve-function.
+ * Stores stuff.
  *
  * @module storage
  */
-(function(){
-  'use strict';
+;(function () {
+  'use strict'
 
-  /*****************************************************************************
+  /**
    * imports
    */
-  var typeCheck = require('type-check').typeCheck;
-  var _ = require('lodash');
-  var serialize = require('serialize-javascript');
+  var typeCheck = require('type-check').typeCheck
+  var _ = require('lodash')
+  var serialize = require('serialize-javascript')
 
-  var redis = require('redis');
+  var redis = require('redis')
 
-  var DEFAULT_HOST = 'localhost';
-  var DEFAULT_PORT = '6379';
+  var redisAdapter = require('./lib/redis-adapter')
+  var postgresqlAdapter = require('./lib/postgresql-adapter')
 
-  var SUPPORTED_ACTIONS = {
+  var DEFAULT_HOST = 'localhost'
+  var DEFAULT_PORT = '6379'
+
+  var ACTION = {
     GET: 'get',
     SET: 'set',
-    DELETE: 'delete'
-  };
+    DELETE: 'delete',
+    INIT: 'init'
+  }
 
-  /*****************************************************************************
+  var MEDIUM = {
+    REDIS: 'redis',
+    POSTGRESQL: 'postgresql'
+  }
+
+  /**
    * exports
    */
-  module.exports = storage;
+  module.exports = storage
 
   /**
    * Provide an action and associated options. Can get, set, and delete values.
@@ -38,43 +47,47 @@
    * @param {options} options contains all function parameters
    * @param {Function} callback handles results
    */
-  function storage(options, callback){
-
-    if( ! options || ! options.action ) {
-
-      callback(null,SUPPORTED_ACTIONS);
-      return;
+  function storage (options, callback) {
+    if (!options || !options.action) {
+      callback(null, ACTION)
+      return
     }
 
-    var error = optionsAreInvalid(options);
+    var error = optionsAreInvalid(options)
 
-    var action = options.action;
+    var action = options.action
 
-    if(error){
-      callback(error);
-      return;
+    if (error) {
+      callback(error)
+      return
     }
 
-    switch(action){
+    switch (action) {
+      case ACTION.SET:
+        memorize(options, callback)
+        break
 
-      case 'set':
-        memorize(options, callback);
-        break;
+      case ACTION.GET:
+        recall(options, callback)
+        break
 
-      case 'get':
-        recall(options, callback);
-        break;
+      case ACTION.DELETE:
+        forget(options, callback)
+        break
 
-      case 'delete':
-        forget(options, callback);
-        break;
+      case ACTION.INIT:
+        init(options, callback)
+        break
+
+      default:
+
+        break
     }
   }
 
-  /*****************************************************************************
+  /**
    * Define helper functions
    */
-
 
   /**
    *
@@ -93,15 +106,14 @@
    * @param {options} options contains key and value
    * @param {Function} callback handles results
    */
-  function memorize(options,callback){
+  function memorize (options, callback) {
+    options.value = serialize(options.value)
 
-    options.value = serialize(options.value);
+    var redisClient = initializeRedisClient(options)
 
-    var redisClient = initializeRedisClient(options);
+    redisClient.set(options.key, options.value, callback)
 
-    redisClient.set(options.key, options.value, callback);
-
-    redisClient.quit();
+    redisClient.quit()
   }
 
   /**
@@ -110,18 +122,16 @@
    * @param {options} options contains key
    * @param {Function} callback handles results
    */
-  function recall(options,callback){
+  function recall (options, callback) {
+    var redisClient = initializeRedisClient(options)
 
-    var redisClient = initializeRedisClient(options);
+    redisClient.get(options.key, function (error, response) {
+      response = JSON.parse(response)
 
-    redisClient.get(options.key, function(error, response){
+      callback(error, response)
+    })
 
-      response = JSON.parse(response);
-
-      callback(error,response);
-    });
-
-    redisClient.quit();
+    redisClient.quit()
   }
 
   /**
@@ -130,16 +140,13 @@
    * @param {options} options contains key
    * @param {Function} callback handle results
    */
-  function forget(options, callback){
+  function forget (options, callback) {
+    var redisClient = initializeRedisClient(options)
 
-    var redisClient = initializeRedisClient(options);
+    redisClient.DEL(options.key, callback)
 
-    redisClient.DEL(options.key, callback);
-
-    redisClient.quit();
+    redisClient.quit()
   }
-
-
 
   /**
    * Validate inputs.
@@ -150,32 +157,31 @@
    *
    * @returns {Error|Boolean} any errors due to invalid inputs
    */
-  function optionsAreInvalid(options){
+  function optionsAreInvalid (options) {
+    var message
+    var action = options.action
 
-    var message;
-    var action = options.action;
+    var actionIsString = typeCheck('String', action)
 
-    var actionIsString = typeCheck('String',action);
+    if (!actionIsString) {
+      message = 'invalid action: ' + action
 
-    if(!actionIsString){
-      message = 'invalid action: ' + action;
+      message += '. should be a String'
 
-      message += '. should be a String';
-
-      return new Error(message);
+      return new Error(message)
     }
 
-    var supportedActions = _.values(SUPPORTED_ACTIONS);
+    var supportedActions = _.values(ACTION)
 
-    var actionIsSupported = _.includes(supportedActions,action);
+    var actionIsSupported = _.includes(supportedActions, action)
 
-    if(!actionIsSupported){
-      message = 'unsupported action: ' + action;
+    if (!actionIsSupported) {
+      message = 'unsupported action: ' + action
 
-      return new Error(message);
+      return new Error(message)
     }
 
-    return false;
+    return false
   }
 
   /**
@@ -186,28 +192,44 @@
    * @param {options} options optionally contains host and port
    * @returns {Error|Boolean} An error if options are invalid, false otherwise.
    */
-  function initializeRedisClient(options){
-
+  function initializeRedisClient (options) {
     var redisOptions = {
       host: DEFAULT_HOST,
       port: DEFAULT_PORT
-    };
+    }
 
-    if( options.host && options.port ){
-
+    if (options.host && options.port) {
       redisOptions = {
         host: options.host,
         port: options.port
-      };
+      }
     }
 
-    if(options.test){
-
-      var redis = require('fakeredis');
-    } else {
-      redis = require('redis');
+    if (options.test) {
+      var redis = require('fakeredis')
     }
 
-    return redis.createClient(redisOptions);
+    return redis.createClient(redisOptions)
   }
-})();
+
+  /**
+   *
+   * @param options
+   * @param callback
+   */
+  function init (options, callback) {
+    var medium = options.medium
+
+    switch (medium) {
+      case MEDIUM.REDIS:
+        redisAdapter.init(options, callback)
+        break
+
+      case MEDIUM.POSTGRESQL:
+        postgresqlAdapter.init(options,callback)
+        break
+    }
+  }
+
+
+})()
--- test/test-storage.js
+++ test/test-storage.js
@@ -3,179 +3,154 @@
  */
 ;(function () {
   /* global describe, it, beforeEach */
-  'use strict';
+  'use strict'
 
   /***************************************************************************
    * Imports
    */
-  var expect = require('chai').expect;
+  var expect = require('chai').expect
 
-  var storage = require('../storage');
+  var storage = require('../storage')
 
   /***************************************************************************
    * Tests
    */
   describe('storage', function () {
-
-    var options;
-    var TEST_KEY;
-    var TEST_VALUE;
+    var options
+    var TEST_KEY
+    var TEST_VALUE
 
     beforeEach(function () {
-
-      TEST_KEY = 'a key';
+      TEST_KEY = 'a key'
       TEST_VALUE = {
         string: 'a string',
         number: 42,
         object: {
           string: 'another string',
           number: 43,
-          array: [1,2,3]
+          array: [1, 2, 3]
         },
         array: [
           'string',
           {},
-          ['a','b','c']
+          ['a', 'b', 'c']
         ]
-      };
+      }
 
       options = {
         key: TEST_KEY,
         value: TEST_VALUE,
         test: true
-      };
-    });
+      }
+    })
 
     describe('verifying inputs', function () {
-
       it('should return an error if action is not a string', function (done) {
-
-        options.action = {};
+        options.action = {}
 
         storage(options, function (error) {
+          expect(error).to.be.ok
 
-          expect(error).to.be.ok;
+          done()
+        })
+      })
 
-          done();
-        });
-      });
-
-      it('should return something if no options were specified', function(done){
-
-        storage({}, function(error, response) {
-
-          expect(error).to.not.be.ok;
-          expect(response).to.be.ok;
-          done();
-        });
-      });
+      it('should return something if no options were specified', function (done) {
+        storage({}, function (error, response) {
+          expect(error).to.not.be.ok
+          expect(response).to.be.ok
+          done()
+        })
+      })
 
       it('should return an error for invalid action', function (done) {
-
-        options.action = 'foo';
-
-        storage(options, function (error) {
-
-          expect(error).to.be.ok;
-
-          done();
-        });
-      });
-    });
-
-    describe('providing non-default host and port', function(){
-
-      it('should use the provided host and port', function(done){
-
-        options.host = 'localhost';
-        options.port = 6379;
-        options.action = 'set';
-
-        storage(options, function(error) {
-
-          expect(error).to.not.be.ok;
-
-          options.action = 'get';
-
-          storage(options, function(error, result) {
-
-            expect(error).to.not.be.ok;
-            expect(result).to.deep.equal(TEST_VALUE);
-
-            options.action = 'delete';
-
-            storage(options, function(error) {
-
-              expect(error).to.not.be.ok;
-
-              options.action = 'get';
-
-              storage(options, function(error, result) {
-
-                expect(error).to.not.be.ok;
-
-                expect(result).to.be.null;
-
-                done();
-              });
-            });
-          });
-        });
-      });
-    });
-
-    describe('set', function () {
-
-      it('should set values', function (done) {
-
-        options.action = 'set';
+        options.action = 'foo'
 
         storage(options, function (error) {
+          expect(error).to.be.ok
 
-          expect(error).to.not.be.ok;
+          done()
+        })
+      })
+    })
 
-          done();
-        });
-      });
-    });
-
-    describe('get', function () {
-
-      it('should get values', function (done) {
-
-        options.action = 'get';
-
-        storage(options, function (error, result) {
-
-          expect(error).to.not.be.ok;
-          expect(result).to.deep.equal(TEST_VALUE);
-
-          done();
-        });
-      });
-    });
-
-    describe('delete', function () {
-
-      it('should delete values', function (done) {
-
-        options.action = 'delete';
+    describe('providing non-default host and port', function () {
+      it('should use the provided host and port', function (done) {
+        options.host = 'localhost'
+        options.port = 6379
+        options.action = 'set'
 
         storage(options, function (error) {
+          expect(error).to.not.be.ok
 
-          expect(error).to.not.be.ok;
-
-          options.action = 'get';
+          options.action = 'get'
 
           storage(options, function (error, result) {
+            expect(error).to.not.be.ok
+            expect(result).to.deep.equal(TEST_VALUE)
 
-            expect(error).to.not.be.ok;
-            expect(result).to.be.null;
+            options.action = 'delete'
 
-            done();
-          });
-        });
-      });
-    });
-  });
-})();
+            storage(options, function (error) {
+              expect(error).to.not.be.ok
+
+              options.action = 'get'
+
+              storage(options, function (error, result) {
+                expect(error).to.not.be.ok
+
+                expect(result).to.be.null
+
+                done()
+              })
+            })
+          })
+        })
+      })
+    })
+
+    describe('set', function () {
+      it('should set values', function (done) {
+        options.action = 'set'
+
+        storage(options, function (error) {
+          expect(error).to.not.be.ok
+
+          done()
+        })
+      })
+    })
+
+    describe('get', function () {
+      it('should get values', function (done) {
+        options.action = 'get'
+
+        storage(options, function (error, result) {
+          expect(error).to.not.be.ok
+          expect(result).to.deep.equal(TEST_VALUE)
+
+          done()
+        })
+      })
+    })
+
+    describe('delete', function () {
+      it('should delete values', function (done) {
+        options.action = 'delete'
+
+        storage(options, function (error) {
+          expect(error).to.not.be.ok
+
+          options.action = 'get'
+
+          storage(options, function (error, result) {
+            expect(error).to.not.be.ok
+            expect(result).to.be.null
+
+            done()
+          })
+        })
+      })
+    })
+  })
+})()
